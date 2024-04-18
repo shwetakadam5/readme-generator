78fd8007ed70140d251b72b2ccb4cac42ed758b2
shwetakadam5 <159270090+shwetakadam5@users.noreply.github.com>
2024-04-19 00:08:19 +1000

    Updated the functions init and questions : Initial Version

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const colors = require("colors");
+const fs = require('fs/promises');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    [ "input", "titleInput", "What is your project title?", "defaulttitle" ],
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+
+    const answer = await inquirer
+        .prompt(
+            questions.map((question) => {
+                return {
+                    type: question[ 0 ],
+                    name: question[ 1 ],
+                    message: question[ 2 ],
+                    default: question[ 3 ],
+                }
+            }));
+    console.log(answer);
+}
 
 // Function call to initialize app
 init();
